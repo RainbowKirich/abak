ded670298e664667a7797e8fe207dcacc9b5156b
Kirich <llirik@bk.ru>
2015-03-29 21:47:30 +0500

    pervii commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abak::Application.config.secret_token = '803fe4204034a92c73dc191ac15d4fdb081f3edb8b1f30c52d1e36d0a589d8f071cddf448810aaa2d17f8462daa174ac678dab6444a2c2c927f917dac045cb19'
